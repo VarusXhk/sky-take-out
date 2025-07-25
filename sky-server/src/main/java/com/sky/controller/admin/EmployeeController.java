1d9a33b0e966ae340082aea613443ffae3d29ddc
varus <1240324817@qq.com>
2025-07-25 22:19:01 +0800

    update the implementations of modifying the status of an employee

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -90,4 +90,12 @@ public class EmployeeController {
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
     }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result startOrStop(@PathVariable int status, long id){
+        log.info("启用禁用员工账号， 参数为：{}， {}", status, id);
+        employeeService.startOrStop(status, id);
+        return Result.success();
+    }
 }
